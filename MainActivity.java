714c5eb0bfa685c88ce7b444a2ec01eb6632e37d
ujwala45 <shettyujwala45@gmail.com>
2026-05-19 10:38:21 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.program13;
 
+import android.Manifest;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,24 +20,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         location = findViewById(R.id.location);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
     }
 
-    public void showMap(View v) {
+    // GIVEN LOCATION
+    public void givenLocation(View v) {
 
         String loc = location.getText().toString();
 
-        // Validation
         if(loc.isEmpty()) {
-            Toast.makeText(this, "Enter Location", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,
+                    "Enter Location",
+                    Toast.LENGTH_SHORT).show();
             return;
         }
 
-        // Open Google Map
         Uri uri = Uri.parse("geo:0,0?q=" + loc);
-        Intent i = new Intent(Intent.ACTION_VIEW, uri);
 
-        startActivity(i);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
 
-        Toast.makeText(this, "Opening Map...", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,
+                "Opening Map",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    // CURRENT LOCATION
+    public void currentLocation(View v) {
+
+        try {
+
+            LocationManager lm =
+                    (LocationManager)getSystemService(LOCATION_SERVICE);
+
+            Location loc =
+                    lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            double lat = loc.getLatitude();
+            double lon = loc.getLongitude();
+
+            Uri uri = Uri.parse("geo:"+lat+","+lon);
+
+            startActivity(new Intent(Intent.ACTION_VIEW, uri));
+
+            Toast.makeText(this,
+                    "Showing Current Location",
+                    Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+
+            Toast.makeText(this,
+                    "Turn ON Location",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
